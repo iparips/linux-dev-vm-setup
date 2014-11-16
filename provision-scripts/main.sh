00fd4217097fb02b353e4c055ee29d96f0031b32
Ilya Paripsa <iparips@gmail.com>
2014-11-17 08:05:13 +1100

    Add install of sublime text
    - add break on error & bash verbose
    - fix other errors in the script

--- provision-scripts/main.sh
+++ provision-scripts/main.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-apt-get -y update
-sudo apt-get install build-essential dos2unix git
+# break on error, verbose
+set -e -x
+
+user_home=/home/vagrant 
+
+# apt-get -y update
+sudo apt-get -y install build-essential dos2unix git
 
 # make bashrc and prompt usable in Linux
-dos2unix /home/vagrant/.bashrc
-dos2unix /home/vagrant/.prompt
+dos2unix ${user_home}/.bashrc
+dos2unix ${user_home}/.prompt
 
 # install ruby-install
 wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
@@ -14,10 +19,16 @@ cd ruby-install-0.5.0/
 sudo make install
 
 # install ruby
-ruby-install ruby 2.1.4
+rm -rf ${user_home}/.rubies 
+mkdir ${user_home}/.rubies  
+ruby-install -r ${user_home}/.rubies ruby 2.1.5
 
 # install chruby
 wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
 tar -xzvf chruby-0.3.8.tar.gz
 cd chruby-0.3.8/
 sudo make install
+
+# install sublime text
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get install sublime-text 
